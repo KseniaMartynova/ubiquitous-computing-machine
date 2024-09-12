819c3bbba9ebb701598017a3159cff2f0ad4ee6e
Ksenia Martynova <ksenia.m4rtinova@yandex.ru>
2024-09-12 10:58:37 +0000

    provercka

---
+++ lapack-openblas/ladock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Build the Docker container
+echo "Building Docker container..."
+docker build -t lapack:latest -f Dockerfile.lablas .
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker build completed successfully."
+else
+    echo "Docker build failed."
+fi
