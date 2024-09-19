305f54c21e7ec35707704da1c0f16c0dadaea1f1
Ksenia Martynova <ksenia.m4rtinova@yandex.ru>
2024-09-19 13:29:22 +0000

    cuda

--- cuda/cudarun.sh
+++ cuda/cudarun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define the list of numbers to cycle through
-numbers=(100 500 1000)
+numbers=(20000)
 
 # Use a for loop to iterate over the array and run the command for each number, 10 times
 for num in "${numbers[@]}"; do
     echo "Running docker run cuda $num"
     for i in {1..10}; do
-        nvidia-docker run cud "$num"
+        nvidia-docker run cudaks "$num"
     done
 done
