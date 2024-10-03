4a8379f23acf93f415d77b16a7e09f6ea05ae6f7
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-03 12:40:14 +0300

    Update runAll.sh

--- run/runAll.sh
+++ run/runAll.sh
@@ -28,3 +28,13 @@ for n in "${numbers[@]}"; do
         docker run num "$n"
     done
 done
+
+
+numbers=(100 500 1000 5000 10000 20000)
+# Use a for loop to iterate over the array and run the command for each number, 10 times
+for num in "${numbers[@]}"; do
+    echo "Running docker run eigen $num"
+    for i in {1..10}; do
+        docker run eigen "$num"
+    done
+done
