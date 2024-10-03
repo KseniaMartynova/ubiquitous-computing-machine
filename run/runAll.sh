62830456fd63c1e81bfbd64944c18f2645a8b320
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-03 12:38:49 +0300

    Update runAll.sh

--- run/runAll.sh
+++ run/runAll.sh
@@ -9,3 +9,13 @@ for num in "${numbers[@]}"; do
     done
 done
 
+
+numbers=(20000)
+# Use a for loop to iterate over the array and run the command for each number, 10 times
+for num in "${numbers[@]}"; do
+    echo "Running docker run intel/oneapi-basekit $num"
+    for i in {1..10}; do
+        docker run intel/oneapi-basekit "$num"
+    done
+done
+
