4cedf497b6193a713d5e60e17ab1fa567388f7ab
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-03 12:16:54 +0300

    Update runAllDocker.sh

--- runAllDocker.sh
+++ runAllDocker.sh
@@ -30,3 +30,13 @@ if [ $? -eq 0 ]; then
 else
     echo "Docker build failed."
 fi
+
+echo "Building Docker container..."
+docker build -t num:latest -f Dockerfile.num .
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker build completed successfully."
+else
+    echo "Docker build failed."
+fi
