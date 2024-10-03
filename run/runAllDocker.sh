2b4209c1e31a1a20bbd35c529e68037c495ff4b3
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-03 16:58:24 +0300

    Update runAllDocker.sh

--- run/runAllDocker.sh
+++ run/runAllDocker.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 # Build the Docker container
-#Eigen
-echo "Building Docker container..."
-docker build -t eigen:latest -f Dockerfile.Eigen .
-# Check if the build was successful
-if [ $? -eq 0 ]; then
-    echo "Docker build completed successfully."
-else
-    echo "Docker build failed."
-fi
-
 #Lapack-OpenBlas
 echo "Building Docker container..."
 docker build -t lapack:latest -f Dockerfile.lablas .
