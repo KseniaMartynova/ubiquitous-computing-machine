4c24a8d9aaaff4e7bb00079197dc602068eb1b64
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-03 12:19:22 +0300

    Update runAllDocker.sh

--- run/runAllDocker.sh
+++ run/runAllDocker.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 # Build the Docker container
+#Eigen
 echo "Building Docker container..."
 docker build -t eigen:latest -f Dockerfile.Eigen .
-
 # Check if the build was successful
 if [ $? -eq 0 ]; then
     echo "Docker build completed successfully."
@@ -11,9 +10,9 @@ else
     echo "Docker build failed."
 fi
 
+#Lapack-OpenBlas
 echo "Building Docker container..."
 docker build -t lapack:latest -f Dockerfile.lablas .
-
 # Check if the build was successful
 if [ $? -eq 0 ]; then
     echo "Docker build completed successfully."
@@ -21,9 +20,9 @@ else
     echo "Docker build failed."
 fi
 
+#MKL
 echo "Building Docker container..."
 docker build -t intel/oneapi-basekit:latest -f Dockerfile.mkl .
-
 # Check if the build was successful
 if [ $? -eq 0 ]; then
     echo "Docker build completed successfully."
@@ -31,9 +30,9 @@ else
     echo "Docker build failed."
 fi
 
+#Numpy
 echo "Building Docker container..."
 docker build -t num:latest -f Dockerfile.num .
-
 # Check if the build was successful
 if [ $? -eq 0 ]; then
     echo "Docker build completed successfully."
