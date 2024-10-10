42988bf16786cf24774fa1b7020382c3db361a65
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-10 16:57:01 +0300

    Update mkrun.sh

--- mkl/mkrun.sh
+++ mkl/mkrun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define the list of numbers to cycle through
-numbers=(100)
+numbers=(100 500 1000)
 
 # Use a for loop to iterate over the array and run the command for each number, 10 times
 for num in "${numbers[@]}"; do
-    echo "Running docker run mklqr $num"
+    echo "Running docker run mkllu $num"
     for i in {1..10}; do
-        docker run mklqr "$num"
+        docker run mkllu "$num"
     done
 done
