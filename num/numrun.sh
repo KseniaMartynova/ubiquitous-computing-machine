d90108cfb85e683749c3405e281c2884ad4ac878
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-10 12:55:11 +0300

    Update numrun.sh

--- num/numrun.sh
+++ num/numrun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define the list of numbers to cycle through
-numbers=(20000)
+numbers=(100 500 1000)
 
 # Use a for loop to iterate over the array and run the command for each number, 10 times
 for n in "${numbers[@]}"; do
     echo "Running docker run num $n"
     for i in {1..10}; do
-        docker run numinv "$n"
+        docker run numsvd "$n"
     done
 done
