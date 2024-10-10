9491ca2c74709007312776a3d7d6240001dba2c0
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-10 14:56:48 +0300

    Update ladock.sh

--- lapack-openblas/ladock.sh
+++ lapack-openblas/ladock.sh
@@ -2,7 +2,7 @@
 
 # Build the Docker container
 echo "Building Docker container..."
-docker build -t lasvd:latest -f Dockerfile.lasvd .
+docker build -t laqr:latest -f Dockerfile.laqr .
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
