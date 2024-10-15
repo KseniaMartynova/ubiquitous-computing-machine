bf0588fad08a9e08b52c917d8c0b24373d37647c
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-15 12:28:02 +0300

    Update mkldock.sh

--- mkl/mkldock.sh
+++ mkl/mkldock.sh
@@ -2,7 +2,7 @@
 
 # Build the Docker container
 echo "Building Docker container..."
-docker build -t mkllu:latest -f Dockerfile.mkllu .
+docker build -t mklmul:latest -f Dockerfile.mklmul .
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
