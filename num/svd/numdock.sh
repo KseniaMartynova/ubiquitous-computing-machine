7b25c84c2a6a542650dd191165320fc14747db5a
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2024-10-15 14:58:08 +0300

    Update and rename num/numdock.sh to num/svd/numdock.sh

--- num/svd/numdock.sh
+++ num/svd/numdock.sh
@@ -2,7 +2,7 @@
 
 # Build the Docker container
 echo "Building Docker container..."
-docker build -t numqr:latest -f Dockerfile.numqr .
+docker build -t numsvd:latest -f Dockerfile.numsvd .
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
