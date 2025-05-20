87a5d1ef509166444bb88c79caf186e230e85679
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2025-05-20 20:39:12 +0300

    Delete i directory

--- i/RUN/post_run.sh
+++
@@ -1,7 +0,0 @@
-
-cd result
-for i in `ls *.txt`; do python ../utils/get_avg_time.py $i; done > ../timing.txt
-for i in `ls *stats.log`; do python ../utils/get_cpu_mem.py $i; done > ../resource_usage.txt
-python ../utils/merge.py ../timing.txt ../resource_usage.txt  > ../end_result.txt
-python ../utils/tables.py ../end_result.txt
-cd ..
