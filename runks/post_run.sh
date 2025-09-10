cb22f6eb9c6f8e4d1f92ac4d72e417b3f3e5a050
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2025-09-10 14:32:20 +0300

    Delete runks/post_run.sh

--- runks/post_run.sh
+++
@@ -1,7 +0,0 @@
-
-cd result
-for i in `ls *.txt`; do python3 ../utils/get_avg_time.py $i; done > ../timing.txt
-for i in `ls *stats.log`; do python3 ../utils/get_cpu_mem.py $i; done > ../resource_usage.txt
-python3 ../utils/merge.py ../timing.txt ../resource_usage.txt  > ../end_result.txt
-python3 ../utils/tables.py ../end_result.txt
-cd ..
