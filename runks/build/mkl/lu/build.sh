eda4c82269541398b9dc75e5f206b93cf49518f2
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2025-12-19 12:26:13 +0300

    Create build.sh

---
+++ runks/build/mkl/lu/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Build the Docker container
+
+
+
+# Function to build Docker container with error handling
+build_container() {
+    local container_name=$1
+    local dockerfile=$2
+
+    echo "Building Docker container: $container_name using $dockerfile..."
+    if ! docker build -t "$container_name" -f "$dockerfile" .; then
+        echo "Error: Building $container_name with $dockerfile failed."
+        exit 1
+    fi
+}
+
+# Lapack-OpenBlas
+
+
+build_container "mkl_lu" "Dockerfile.mkllu"
+
+
+cd ../
+echo "All containers built successfully!"
