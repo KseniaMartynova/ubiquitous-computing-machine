1cce1fd63c0bf400c3d627ae80e0aa4e2c76a2f6
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2026-08-12 17:18:02 +0300

    Update numlu.py

--- runks/build/numpy/lu/numlu.py
+++ runks/build/numpy/lu/numlu.py
@@ -70,7 +70,7 @@ def main():
         print("Matrix size must be a positive integer")
         sys.exit(1)
 
-    matrix = generate_positive_definite_matrix(n)
+    matrix = generate_positive_definite_matrix(n, n)
 
     # Замер времени
     start = time.perf_counter()
