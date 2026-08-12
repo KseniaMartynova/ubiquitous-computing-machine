5df0dc24f5b3fcbced8fa947ab3024761566161e
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2026-08-12 17:08:25 +0300

    Update mklMultiplication.cpp

--- runks/build/mkl/multiplication/mklMultiplication.cpp
+++ runks/build/mkl/multiplication/mklMultiplication.cpp
@@ -15,7 +15,7 @@ std::vector<std::string> called_routines;
 // Генерация симметричной положительно определённой матрицы
 void generate_positive_definite_matrix(double* A, int n, int seed) {
     std::random_device rd;
-    std::mt19937 gen(rd());
+    std::mt19937 gen(seed);
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
     for (int i = 0; i < n * n; ++i) {
