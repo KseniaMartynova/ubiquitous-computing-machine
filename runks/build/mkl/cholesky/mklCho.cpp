d32ba487550ab3935a95c77537fd7ec300333c1b
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2026-08-12 17:04:04 +0300

    Update mklCho.cpp

--- runks/build/mkl/cholesky/mklCho.cpp
+++ runks/build/mkl/cholesky/mklCho.cpp
@@ -14,7 +14,7 @@
 std::vector<std::string> called_routines;
 
 
-void generate_positive_definite_matrix(double* A, int n) {
+void generate_positive_definite_matrix(double* A, int n, int seed) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0.0, 1.0);
