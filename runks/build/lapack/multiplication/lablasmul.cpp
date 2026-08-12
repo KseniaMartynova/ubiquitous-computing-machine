c8ddfdfaa97157de30333cf8a0799d51cbb9a62a
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2026-08-12 17:06:05 +0300

    Update lablasmul.cpp

--- runks/build/lapack/multiplication/lablasmul.cpp
+++ runks/build/lapack/multiplication/lablasmul.cpp
@@ -14,8 +14,7 @@ std::vector<std::string> called_routines;
 // Создание положительно определённой матрицы
 std::vector<double> create_positive_definite_matrix(int n, int seed)  {
     std::vector<double> matrix(n * n);
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    std::mt19937 gen(seed);
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
     for (int i = 0; i < n; ++i)
